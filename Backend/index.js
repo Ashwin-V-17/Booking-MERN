9bac09bab9c3b031f70a39915aef0792f2c7eb9a
Ashwin-V-17 <ashwinv1707@gmail.com>
2024-03-10 11:43:50 +0530

    created sample apis

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose  from "mongoose";
+import authRoutes from "./routes/auth.js"
+import usersRoutes from "./routes/users.js"
+import hotelsRoutes from "./routes/hotels.js"
+import roomsRoutes from "./routes/rooms.js"
 const app=express();
 dotenv.config()
 const connect = async ()=>{
@@ -20,7 +24,11 @@ mongoose.connection.on("disconnected",()=>{
 mongoose.connection.on("connected",()=>{
     console.log("mongoDB connected!");
 })
-
+//middlewares
+app.use("/api/auth",authRoutes);
+app.use("/api/auth",usersRoutes);
+app.use("/api/auth",hotelsRoutes);
+app.use("/api/auth",roomsRoutes);
 app.listen(8800,()=>{
     connect()
     console.log("connected to Backend");
---
+++ Backend/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const router=express();
+
+router.get("/",(req,res)=>{
+    res.send("Hello,this is auth endpoint");
+})
+router.get("/register",(req,res)=>{
+    res.send("hello,this is auth register endpoint");
+})
+export default router;
